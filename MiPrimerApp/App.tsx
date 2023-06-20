57e29d643833ca9588787c677a9c72a93c2bb17a
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-06-20 14:30:53 -0300

    flex-dirreccion

--- MiPrimerApp/App.tsx
+++ MiPrimerApp/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 // import {CounterScren} from './src/screens/CounterScren';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 import {SafeAreaView} from 'react-native';
+import {FlexScren} from './src/screens/FlexScren';
 // import {DimensionesScreen} from './src/screens/DimensionesScreen';
-import {Position} from './src/screens/Position';
+// import {Position} from './src/screens/Position';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
-      <Position />
+      {/* <Position /> */}
+      <FlexScren />
     </SafeAreaView>
   );
 };
