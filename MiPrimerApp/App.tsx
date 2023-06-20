9e7f985358a88cec61010f460bca274e6e3e6ac8
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-06-20 13:55:37 -0300

    css react-native

--- MiPrimerApp/App.tsx
+++ MiPrimerApp/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 // import {CounterScren} from './src/screens/CounterScren';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 import {SafeAreaView} from 'react-native';
-import {DimensionesScreen} from './src/screens/DimensionesScreen';
+// import {DimensionesScreen} from './src/screens/DimensionesScreen';
+import {Position} from './src/screens/Position';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <BoxObjectModelScreen /> */}
-      <DimensionesScreen />
+      {/* <DimensionesScreen /> */}
+      <Position />
     </SafeAreaView>
   );
 };
