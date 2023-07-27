f634410482b24a7b0a4dee456e798d522c09a83e
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-07-27 11:05:08 -0300

    flex1

--- MiPrimerApp/App.tsx
+++ MiPrimerApp/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 // import {CounterScren} from './src/screens/CounterScren';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 import {SafeAreaView} from 'react-native';
-// import {FlexScren} from './src/screens/FlexScren';
+import {FlexScren} from './src/screens/FlexScren';
 // import {TareaScreen} from './src/screens/TareaScreen';
 // import {DimensionesScreen} from './src/screens/DimensionesScreen';
-import {Position} from './src/screens/Position';
+// import {Position} from './src/screens/Position';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
-      <Position />
-      {/* <FlexScren /> */}
+      {/* <Position /> */}
+      <FlexScren />
       {/* <TareaScreen /> */}
     </SafeAreaView>
   );
